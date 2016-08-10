38ba975078553aa96deb382a1795c4cfb65901b0
Camilo Rodriguez <Camilo Rodriguez>
2016-08-10 07:16:41 -0500

    Disable scene camera for player and add localCamera for each player

--- Assets/_Scripts/Players/PlayerNetworkSetup.cs
+++ Assets/_Scripts/Players/PlayerNetworkSetup.cs
@@ -6,20 +6,23 @@ public class PlayerNetworkSetup : NetworkBehaviour {
 
     PlayerController playerController;
     MeshRenderer playerRender;
+    GameObject sceneCamera;
 
     void Start () {
         playerController = GetComponent<PlayerController>();
         if (!isLocalPlayer) {
             playerController.enabled = false;
         }
-	}
+    }
 
     public override void OnStartLocalPlayer() {
         base.OnStartLocalPlayer();
 
         playerRender = GetComponent<MeshRenderer>();
-
         playerRender.material.color = Color.blue;
+
+        sceneCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        sceneCamera.SetActive(false);
     }
 
 }
